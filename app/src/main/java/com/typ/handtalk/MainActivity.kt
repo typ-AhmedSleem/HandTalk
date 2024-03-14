28f19f34c8740fb28f2b03812c106b5b7cd86f39
typ-AhmedSleem <theyoungprogrammer67@gmail.com>
2024-03-14 21:00:35 +0200

    repo: implemented mediapipe android gesture recognizer example to start on top of it

--- app/src/main/java/com/typ/handtalk/MainActivity.kt
+++ app/src/main/java/com/typ/handtalk/MainActivity.kt
@@ -1,20 +1,17 @@
 package com.typ.handtalk
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.typ.handtalk.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+//    private val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
