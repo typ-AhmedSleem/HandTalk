0fed282edff0443e15df41ef2bf08a1004da8ab9
typ-AhmedSleem <theyoungprogrammer67@gmail.com>
2024-03-14 10:22:20 +0200

    gradle: added project dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,6 +29,9 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        viewBinding = true
+    }
     dependenciesInfo {
         includeInApk = false
         includeInBundle = false
@@ -42,7 +45,12 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation(libs.androidx.camera.core)
+    implementation(libs.androidx.camera.camera2)
+    implementation(libs.androidx.camera.lifecycle)
+    implementation(libs.androidx.camera.view)
+    implementation(libs.androidx.window)
+    implementation(libs.mediapipe.tasks.vision)
+
 }
