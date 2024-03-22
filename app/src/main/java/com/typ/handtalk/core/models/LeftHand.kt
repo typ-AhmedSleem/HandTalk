8740e0f1885eb95d028e2843652e498a960ef4f4
typ-AhmedSleem <theyoungprogrammer67@gmail.com>
2024-03-22 09:52:47 +0200

    refactor: deleted LeftHand and RightHand classes

--- app/src/main/java/com/typ/handtalk/core/models/LeftHand.kt
+++
@@ -1,5 +0,0 @@
-package com.typ.handtalk.core.models
-
-import com.google.mediapipe.tasks.components.containers.NormalizedLandmark
-
-class LeftHand(sign: HandSign?, landmarks: List<List<NormalizedLandmark>>) : Hand(idx = 0, sign, landmarks)
