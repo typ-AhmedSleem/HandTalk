b3dbeb643f6df7c286bdff6189f69a7063f0f10f
typ-AhmedSleem <theyoungprogrammer67@gmail.com>
2024-03-25 02:57:10 +0200

    a2s-translator: started feature by creating ui #1

---
+++ app/src/main/java/com/typ/handtalk/ui/activities/Arabic2SignTranslatorActivity.kt
@@ -0,0 +1,15 @@
+package com.typ.handtalk.ui.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.typ.handtalk.databinding.ActivityArabic2signTranslatorBinding
+
+class Arabic2SignTranslatorActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityArabic2signTranslatorBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
