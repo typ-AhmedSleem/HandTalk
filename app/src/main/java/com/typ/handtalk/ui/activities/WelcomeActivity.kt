822d5bd2814c1feecdfe72cdff5bebb98c498376
typ-AhmedSleem <theyoungprogrammer67@gmail.com>
2024-04-20 21:20:41 +0200

    refactor: made some changes in Articles feature

--- app/src/main/java/com/typ/handtalk/ui/activities/WelcomeActivity.kt
+++ app/src/main/java/com/typ/handtalk/ui/activities/WelcomeActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.typ.handtalk.databinding.ActivityWelcomeBinding
+import com.typ.handtalk.ui.a2s.Arabic2SignTranslatorActivity
 import com.typ.handtalk.ui.articles.ArticlesHubActivity
 
 class WelcomeActivity : AppCompatActivity() {
@@ -14,11 +15,12 @@ class WelcomeActivity : AppCompatActivity() {
             setContentView(root)
             btnStartApp.setOnClickListener {
                 startActivity(Intent(this@WelcomeActivity, LiveSignTranslatorActivity::class.java))
-//                finish()
             }
-            btnStartApp.setOnLongClickListener {
+            btnArabic2Sign.setOnClickListener {
+                startActivity(Intent(this@WelcomeActivity, Arabic2SignTranslatorActivity::class.java))
+            }
+            btnArticles.setOnClickListener {
                 startActivity(Intent(this@WelcomeActivity, ArticlesHubActivity::class.java))
-                true
             }
         }
     }
