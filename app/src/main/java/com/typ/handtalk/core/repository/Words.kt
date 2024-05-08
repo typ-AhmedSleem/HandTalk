37257cb32544bec57f9e53ec1658b123a55cd8ca
typ-AhmedSleem <theyoungprogrammer67@gmail.com>
2024-05-08 13:55:14 +0300

    s2a-translator: added new numbering of signs to the words

--- app/src/main/java/com/typ/handtalk/core/repository/Words.kt
+++ app/src/main/java/com/typ/handtalk/core/repository/Words.kt
@@ -11,83 +11,94 @@ object Words {
             1,
             "السلام عليكم ورحمة الله وبركاته",
             sequenceOfGestures(
-                rhResult("11"),
-                rhResult("19"), // 19
-                rhResult("2"),
-                rhResult("5"),
-                rhResult("4"),
-                rhResult("6"),
+                rhResult("48"),
+                rhResult("31"), // 19
+                rhResult("32"),
+                rhResult("33"),
+                rhResult("41"),
+                rhResult("49"),
             )
         ),
         Word(
             2,
             "صباح الخير",
             sequenceOfGestures(
-                rhResult("7"),
-                rhResult("9"),
-                rhResult("19"),
-                rhResult("11"),
+                rhResult("44"),
+                rhResult("40"),
+                rhResult("38"),
+                rhResult("37"),
             )
         ),
         Word(
             3,
             "ممكن",
             sequenceOfGestures(
-                rhResult("19"), // 12
+                rhResult("35"), // 12
             )
         ),
         Word(
             4,
             "كليه",
             sequenceOfGestures(
-                rhResult("17"),
-                rhResult("18"),
+                rhResult("43"),
+                rhResult("47"),
             )
         ),
         Word(
             5,
             "الم", sequenceOfGestures(
-                rhResult("29"),
-                rhResult("30"),
+                rhResult("17"),
+                rhResult("23"),
             )
         ),
         Word(
             6,
             "شكراً", sequenceOfGestures(
-                rhResult("11"), // todo: change to 13
+                rhResult("36"),
             )
         ),
         Word(
             7,
             "دكتور", sequenceOfGestures(
-                rhResult("23"),
-                rhResult("24"),
+                rhResult("19"),
+                rhResult("14"),
             )
         ),
         Word(
             8,
             "المعده", sequenceOfGestures(
-                rhResult("40"),
+                rhResult("18"),
             )
         ),
         Word(
             9,
             "باطنة", sequenceOfGestures(
-                rhResult("48"),
-                rhResult("47"),
+                rhResult("7"),
             )
         ),
         Word(
             10,
             "مكان", sequenceOfGestures(
-                rhResult("38"),
-                rhResult("41"),
+                rhResult("20"),
+                rhResult("26"),
             )
         ),
         Word(
             11,
             "في", sequenceOfGestures(
-                rhResult("28"),
+                rhResult("13"),
+            )
+        ),
+        Word(
+            12,
+            "فين", sequenceOfGestures(
+                rhResult("50"),
+            )
+        ),
+        Word(
+            13,
+            "سؤال", sequenceOfGestures(
+                rhResult("51"),
             )
         ),
     )
