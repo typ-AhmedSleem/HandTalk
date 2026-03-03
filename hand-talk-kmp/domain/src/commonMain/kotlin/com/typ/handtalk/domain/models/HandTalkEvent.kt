76bb57c16837171ba7ee2bbd26bc3d88817801b8
typ-AhmedSleem <theyoungprogrammer67@gmail.com>
2026-03-03 21:04:35 +0200

    feat(domain): add StopRecognizer event to HandTalkEvent
    Introduced a new `StopRecognizer` event to the `HandTalkEvent` sealed class.
    
    - Added `StopRecognizer` object to signal the recognizer activity to finish.
    - This event is intended for internal use when `HandTalk.stop` is invoked.

--- hand-talk-kmp/domain/src/commonMain/kotlin/com/typ/handtalk/domain/models/HandTalkEvent.kt
+++ hand-talk-kmp/domain/src/commonMain/kotlin/com/typ/handtalk/domain/models/HandTalkEvent.kt
@@ -4,4 +4,7 @@ sealed class HandTalkEvent {
     data class SignRecognized(val lhs: HandSign?, val rhs: HandSign?) : HandTalkEvent()
     data class SignChanged(val old: HandSign, val new: HandSign) : HandTalkEvent()
     object HandsDisappeared : HandTalkEvent()
+    
+    /** Internal event emitted by [HandTalk.stop] to signal the recognizer activity to finish. */
+    object StopRecognizer : HandTalkEvent()
 }
